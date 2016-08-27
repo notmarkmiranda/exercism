2235cc4e828b9841c60d00c0a6c1c24e7a8a17b6
Mark Miranda <notmarkmiranda@gmail.com>
2016-08-26 18:10:30 -0600

    finished roman umberals

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,6 +1,23 @@
-class RomanNumerals
+class Fixnum
+
+  RN = {
+    1000 => "M", 900 => "CM", 500 => "D", 400 => "CD", 100 => "C", 90 => "XC",
+    50 => "L", 40 => "XL", 10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I"
+  }
 
   def to_roman
-    "I"
+    ret_val = ""
+    current_num = self
+
+    RN.each do |number, roman|
+      while current_num >= number
+        current_num -= number
+        ret_val << roman
+      end
+    end
+
+    ret_val
   end
+
+  VERSION = 1
 end
